e74c293f6c9d4cdbc719ca418cb4aafaeefa9442
JohnLow <jlow1349@gmail.com>
2022-11-26 15:04:13 -0700

    changed name to just "blog"

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "web" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
-  name    = "blog_new"
+  name    = "blog"
   
   vpc_id  = data.aws_vpc.default.id
   
